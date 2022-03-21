528d43f8f6a4d1da2e44832e36227611b4f79b3c
Mona Lisa <carrebola@fpllefia.com>
2022-03-21 13:29:38 +0100

    con foreach

---
+++ app.js
@@ -0,0 +1,51 @@
+var jugador = document.querySelector(".jugadores");
+jugador.addEventListener("click", (elemento) => {
+	var nombre = elemento.target.dataset.nombre;
+	var foto = elemento.target.dataset.foto;
+	if (elemento.target.classList.contains("jugador")) {
+		document.querySelector(".nombre-jugador").innerHTML = nombre;
+
+		document.querySelector(".foto-jugador").innerHTML = `
+                    <img class=""
+							src="${foto}"
+							alt=""
+						/>
+                    `;
+	}
+});
+
+//funcion que devuelve un ul con jugadores
+function getJugadores(jornada = 0, partido = 0) {
+	const nombreEquipoLocal =
+		datos.jornadas[jornada].partidos[partido].local.nombre;
+	const nombreEquipoVisitante =
+		datos.jornadas[jornada].partidos[partido].visitante.nombre;
+	const arrayJugadoresLocal =
+		datos.jornadas[jornada].partidos[partido].local.jugadores;
+	const arrayJugadoresVisitante =
+		datos.jornadas[jornada].partidos[partido].visitante.jugadores;
+
+	var listaJugadoresLocalHTML = `<ul>`;
+	arrayJugadoresLocal.forEach((item) => {
+		console.log(item.nombre);
+		listaJugadoresLocalHTML += `
+            <li class='jugador' data-nombre='${item.nombre}' data-foto = '${item.imagen}'>${item.nombre}</li>
+            `;
+	});
+	listaJugadoresLocalHTML += "<ul>";
+
+	var listaJugadoresVisitanteHTML = `<ul>`;
+	arrayJugadoresVisitante.forEach((item) => {
+		console.log(item.nombre);
+		listaJugadoresVisitanteHTML += `
+            <li class='jugador' data-nombre='${item.nombre}' data-foto = '${item.imagen}'>${item.nombre}</li>
+            `;
+	});
+	listaJugadoresVisitanteHTML += "<ul>";
+
+	console.log(listaJugadoresVisitanteHTML);
+	document.querySelector("ul.local").innerHTML = listaJugadoresLocalHTML;
+	document.querySelector("ul.visitante").innerHTML =
+		listaJugadoresvisitanteHTML;
+}
+getJugadores();
--- src/datos.js
+++ src/datos.js
@@ -1,130 +1,128 @@
 const datos = {
-    liga : 'liga BBVA'
-    jornadas : [
-        {
-            jornada : 1,
-            partidos : [
-                {
-                    id : 1,
-                    resultado : '0 - 4',
-                    local : {
-                        nombre : 'Real Madrid',
-                        jugadores : [
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            }
-                        ]
-                    },
-                    visitante : {
-                        nombre : 'FC Barcelona',
-                        jugadores : [
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            }
-                        ]
-                    }
-        
-                }
-            ]
-        },
-        {
-            jornada : 2,
-            partidos : [
-                {
-                    id : 1,
-                    resultado : '0 - 4',
-                    local : {
-                        nombre : 'Real Madrid',
-                        jugadores : [
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            }
-                        ]
-                    },
-                    visitante : {
-                        nombre : 'FC Barcelona',
-                        jugadores : [
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            },
-                            {
-                                nombre : 'asdf',
-                                imagen : 'asdf.png',
-                                goles : 1,
-                                targetas_amarillas : 0,
-                                targetas_rojas : 0
-                            }
-                        ]
-                    }
-        
-                }
-            ]
-        }
-    ]
-
-}
+	liga: "liga BBVA",
+	jornadas: [
+		{
+			jornada: 1,
+			partidos: [
+				{
+					id: 1,
+					resultado: "0 - 4",
+					local: {
+						nombre: "Real Madrid",
+						jugadores: [
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+						],
+					},
+					visitante: {
+						nombre: "FC Barcelona",
+						jugadores: [
+							{
+								nombre: "Dani Alves",
+								imagen:
+									"https://www.fcbarcelona.com/photo-resources/2021/11/18/9bc6a4f8-70f3-42e1-b935-9e13eec8ff79/08-dani_alves.png?width=300&height=3541",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+						],
+					},
+				},
+			],
+		},
+		{
+			jornada: 2,
+			partidos: [
+				{
+					id: 1,
+					resultado: "0 - 4",
+					local: {
+						nombre: "Real Madrid",
+						jugadores: [
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+						],
+					},
+					visitante: {
+						nombre: "FC Barcelona",
+						jugadores: [
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+							{
+								nombre: "asdf",
+								imagen: "asdf.png",
+								goles: 1,
+								targetas_amarillas: 0,
+								targetas_rojas: 0,
+							},
+						],
+					},
+				},
+			],
+		},
+	],
+};
