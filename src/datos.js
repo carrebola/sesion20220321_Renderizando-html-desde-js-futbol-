06dd2adcffe138a3222d0b32a592a21789a48415
Mona Lisa <carrebola@fpllefia.com>
2022-03-21 13:02:08 +0100

    con json

---
+++ src/datos.js
@@ -0,0 +1,130 @@
+const datos = {
+    liga = 'liga BBVA'
+    jornadas = [
+        {
+            jornada = 1,
+            partidos = [
+                {
+                    id = 1,
+                    resultado = '0 - 4',
+                    local = {
+                        nombre = 'Real Madrid',
+                        jugadores = [
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            }
+                        ]
+                    },
+                    visitante = {
+                        nombre = 'FC Barcelona',
+                        jugadores = [
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            }
+                        ]
+                    }
+        
+                }
+            ]
+        },
+        {
+            jornada = 2,
+            partidos = [
+                {
+                    id = 1,
+                    resultado = '0 - 4',
+                    local = {
+                        nombre = 'Real Madrid',
+                        jugadores = [
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            }
+                        ]
+                    },
+                    visitante = {
+                        nombre = 'FC Barcelona',
+                        jugadores = [
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            },
+                            {
+                                nombre = 'asdf',
+                                imagen = 'asdf.png',
+                                goles = 1,
+                                targetas_amarillas = 0,
+                                targetas_rojas = 0
+                            }
+                        ]
+                    }
+        
+                }
+            ]
+        }
+    ]
+
+}
